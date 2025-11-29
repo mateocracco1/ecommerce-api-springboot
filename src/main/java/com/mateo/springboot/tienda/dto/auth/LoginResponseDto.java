44527aab37aec944df1dfef51fd67dc89b7ea2ce
mateocracco1 <mateonervo@gmail.com>
2025-11-29 19:41:51 -0300

    feat(security): agregar configuración completa de Spring Security y JWT funcionando

--- src/main/java/com/mateo/springboot/tienda/dto/auth/LoginResponseDto.java
+++ src/main/java/com/mateo/springboot/tienda/dto/auth/LoginResponseDto.java
@@ -3,14 +3,13 @@ package com.mateo.springboot.tienda.dto.auth;
 public class LoginResponseDto {
 
     private String token;
-    private String tokenType = "Bearer";
+    private String tokenType = "Bearer  ";
     private String email;
 
     public LoginResponseDto(String token, String email) {
         this.token = token;
         this.email = email;
     }
-
     public String getTokenType() {
         return tokenType;
     }
